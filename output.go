b4312c2ba50cb5f803e785e9d8f3a3ee833bcb9f
Ascheriit <163854943+ascheriit-dkp@users.noreply.github.com>
2026-07-29 14:37:53 +0200

    fix: sanitize and serialize table output

--- output.go
+++ output.go
@@ -1,8 +1,26 @@
+// Copyright 2019 Peter Stöckli
+// Modified in 2026 by Ascheriit-Dkp.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"fmt"
 	"strconv"
+	"strings"
+	"sync"
+	"unicode"
 )
 
 type output interface {
@@ -16,44 +34,130 @@ func newOutput(printAll bool) output {
 
 type tableOutput struct {
 	printAll bool
+	mu       sync.Mutex
 }
 
-func (t tableOutput) PrintHeader() {
-	var header string
-	var args []interface{}
-	if t.printAll {
-		header = "%-9s %-9s %-6s %-42s %-16s %-20s %s\n"
-		args = []interface{}{"TIME", "AF", "PID", "PROCESS", "USER", "DESTINATION", "AS-INFO"}
-	} else {
-		header = "%-9s %-9s %-6s %-34s %-16s %-20s\n"
-		args = []interface{}{"TIME", "AF", "PID", "PROCESS", "USER", "DESTINATION"}
-	}
+func (t *tableOutput) PrintHeader() {
+	t.mu.Lock()
+	defer t.mu.Unlock()
 
-	fmt.Printf(header, args...)
-}
-
-func (t tableOutput) PrintLine(e eventPayload) {
-	time := e.GoTime.Format("15:04:05")
-	dest := e.DestIP.String() + " " + strconv.Itoa(int(e.DestPort))
-
-	var header string
+	var format string
 	var args []interface{}
 
 	if t.printAll {
-		var asText = ""
-		if (ASNameInfo{}) != e.ASNameInfo {
-			asText = "AS" + strconv.Itoa(int(e.ASNameInfo.AsNumber)) + " (" + e.ASNameInfo.Name + ")"
+		format = "%-9s %-9s %-6s %-42s %-16s %-20s %s\n"
+		args = []interface{}{
+			"TIME",
+			"AF",
+			"PID",
+			"PROCESS",
+			"USER",
+			"DESTINATION",
+			"AS-INFO",
 		}
-		header = "%-9s %-9s %-6d %-42s %-16s %-20s %s\n"
-		args = []interface{}{time, e.AddressFamily, e.Pid, e.ProcessPath + " " + e.ProcessArgs, e.User, dest, asText}
 	} else {
-		header = "%-9s %-9s %-6d %-34s %-16s %-20s\n"
-		args = []interface{}{time, e.AddressFamily, e.Pid, e.ProcessPath, e.User, dest}
+		format = "%-9s %-9s %-6s %-34s %-16s %-20s\n"
+		args = []interface{}{
+			"TIME",
+			"AF",
+			"PID",
+			"PROCESS",
+			"USER",
+			"DESTINATION",
+		}
 	}
 
-	fmt.Printf(header, args...)
+	fmt.Printf(format, args...)
 }
 
-func newTableOutput(includeAsNumbers bool) output {
-	return &tableOutput{includeAsNumbers}
+func (t *tableOutput) PrintLine(event eventPayload) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	eventTime := event.GoTime.Format("15:04:05")
+	addressFamily := sanitizeTerminalField(event.AddressFamily)
+	destination := event.DestIP.String() + " " +
+		strconv.Itoa(int(event.DestPort))
+	processPath := sanitizeTerminalField(event.ProcessPath)
+	processArgs := sanitizeTerminalField(event.ProcessArgs)
+	username := sanitizeTerminalField(event.User)
+
+	if t.printAll {
+		process := processPath
+		if processArgs != "" {
+			process += " " + processArgs
+		}
+
+		asText := ""
+		if event.ASNameInfo != (ASNameInfo{}) {
+			asText = "AS" +
+				strconv.Itoa(int(event.ASNameInfo.AsNumber)) +
+				" (" +
+				sanitizeTerminalField(event.ASNameInfo.Name) +
+				")"
+		}
+
+		fmt.Printf(
+			"%-9s %-9s %-6d %-42s %-16s %-20s %s\n",
+			eventTime,
+			addressFamily,
+			event.Pid,
+			process,
+			username,
+			destination,
+			asText,
+		)
+
+		return
+	}
+
+	fmt.Printf(
+		"%-9s %-9s %-6d %-34s %-16s %-20s\n",
+		eventTime,
+		addressFamily,
+		event.Pid,
+		processPath,
+		username,
+		destination,
+	)
+}
+
+// sanitizeTerminalField makes untrusted text safe to print to a terminal.
+//
+// Control characters, newlines, tabs, escape sequences and invisible Unicode
+// formatting characters are rendered as visible escape sequences.
+func sanitizeTerminalField(value string) string {
+	var builder strings.Builder
+
+	for _, character := range value {
+		switch character {
+		case '\n':
+			builder.WriteString(`\n`)
+		case '\r':
+			builder.WriteString(`\r`)
+		case '\t':
+			builder.WriteString(`\t`)
+		case '\\':
+			builder.WriteString(`\\`)
+		default:
+			if unicode.IsGraphic(character) {
+				builder.WriteRune(character)
+				continue
+			}
+
+			if character <= '\uFFFF' {
+				fmt.Fprintf(&builder, `\u%04X`, character)
+			} else {
+				fmt.Fprintf(&builder, `\U%08X`, character)
+			}
+		}
+	}
+
+	return builder.String()
+}
+
+func newTableOutput(includeASNumbers bool) output {
+	return &tableOutput{
+		printAll: includeASNumbers,
+	}
 }
