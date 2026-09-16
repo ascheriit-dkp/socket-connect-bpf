2456cc2b944173becc0e46d6048400f59244e62e
Ascheriit <163854943+ascheriit-dkp@users.noreply.github.com>
2026-09-16 09:09:43 +0200

    dns: correlate TCP lifecycle destinations

--- tcp_lifecycle_enrichment_linux.go
+++ tcp_lifecycle_enrichment_linux.go
@@ -38,6 +38,7 @@ type tcpLifecycleEnrichment struct {
 	Namespaces            *tcpLifecycleNamespacesPayload
 	Container             *tcpLifecycleContainerPayload
 	ASN                   *tcpLifecycleASNPayload
+	DNS                   *dnsCorrelationPayload
 }
 
 type tcpLifecycleEnrichmentLookups struct {
@@ -46,6 +47,7 @@ type tcpLifecycleEnrichmentLookups struct {
 	username    func(uint32) string
 	context     func(int) processContextSnapshot
 	asn         func(tcpLifecycleEventPayload) *tcpLifecycleASNPayload
+	dns         func(tcpLifecycleEventPayload) *dnsCorrelationPayload
 }
 
 type tcpLifecycleEnricher struct {
@@ -56,38 +58,24 @@ type tcpLifecycleEnricher struct {
 	processes             *processCache
 }
 
-func newTCPLifecycleEnricher(
-	includeExtendedFields bool,
-) *tcpLifecycleEnricher {
-	return newTCPLifecycleEnricherWithProcessCache(
-		includeExtendedFields,
-		nil,
-	)
+func newTCPLifecycleEnricher(includeExtendedFields bool) *tcpLifecycleEnricher {
+	return newTCPLifecycleEnricherWithProcessCache(includeExtendedFields, nil)
 }
 
-func newTCPLifecycleEnricherWithProcessCache(
-	includeExtendedFields bool,
-	processes *processCache,
-) *tcpLifecycleEnricher {
+func newTCPLifecycleEnricherWithProcessCache(includeExtendedFields bool, processes *processCache) *tcpLifecycleEnricher {
 	enricher := newTCPLifecycleEnricherWithLookups(
 		includeExtendedFields,
 		maxTCPLifecycleEnrichmentEntries,
 		defaultTCPLifecycleEnrichmentLookups(),
 	)
 	enricher.processes = processes
-
 	return enricher
 }
 
-func newTCPLifecycleEnricherWithLookups(
-	includeExtendedFields bool,
-	maxEntries int,
-	lookups tcpLifecycleEnrichmentLookups,
-) *tcpLifecycleEnricher {
+func newTCPLifecycleEnricherWithLookups(includeExtendedFields bool, maxEntries int, lookups tcpLifecycleEnrichmentLookups) *tcpLifecycleEnricher {
 	if maxEntries < 0 {
 		maxEntries = 0
 	}
-
 	return &tcpLifecycleEnricher{
 		includeExtendedFields: includeExtendedFields,
 		maxEntries:            maxEntries,
@@ -105,12 +93,11 @@ func defaultTCPLifecycleEnrichmentLookups() tcpLifecycleEnrichmentLookups {
 		username: lookupTCPLifecycleUsername,
 		context:  lookupProcessContext,
 		asn:      lookupTCPLifecycleASN,
+		dns:      lookupTCPLifecycleDNS,
 	}
 }
 
-func (enricher *tcpLifecycleEnricher) Enrich(
-	payload tcpLifecycleEventPayload,
-) tcpLifecycleEventPayload {
+func (enricher *tcpLifecycleEnricher) Enrich(payload tcpLifecycleEventPayload) tcpLifecycleEventPayload {
 	enrichment, cached := enricher.connections[payload.ConnectionID]
 	if !cached {
 		enrichment = enricher.lookup(payload)
@@ -120,52 +107,38 @@ func (enricher *tcpLifecycleEnricher) Enrich(
 	payload.ProcessArgs = enrichment.ProcessArgs
 	payload.User = enrichment.User
 	payload.GID = cloneUint32Pointer(enrichment.GID)
-	payload.ProcessStartTimeTicks = cloneUint64Pointer(
-		enrichment.ProcessStartTimeTicks,
-	)
+	payload.ProcessStartTimeTicks = cloneUint64Pointer(enrichment.ProcessStartTimeTicks)
 	payload.Parent = cloneTCPLifecycleParent(enrichment.Parent)
 	payload.Cgroup = cloneTCPLifecycleCgroup(enrichment.Cgroup)
 	payload.Namespaces = cloneTCPLifecycleNamespaces(enrichment.Namespaces)
 	payload.Container = cloneTCPLifecycleContainer(enrichment.Container)
 	payload.ASN = cloneTCPLifecycleASN(enrichment.ASN)
+	payload.DNS = cloneDNSCorrelation(enrichment.DNS)
 
 	switch payload.EventType {
-	case tcpLifecycleEventTypeConnectFailed,
-		tcpLifecycleEventTypeClosed:
+	case tcpLifecycleEventTypeConnectFailed, tcpLifecycleEventTypeClosed:
 		delete(enricher.connections, payload.ConnectionID)
 	default:
 		if cached || len(enricher.connections) < enricher.maxEntries {
 			enricher.connections[payload.ConnectionID] = enrichment
 		}
 	}
-
 	return payload
 }
 
-func (enricher *tcpLifecycleEnricher) lookup(
-	payload tcpLifecycleEventPayload,
-) tcpLifecycleEnrichment {
+func (enricher *tcpLifecycleEnricher) lookup(payload tcpLifecycleEventPayload) tcpLifecycleEnrichment {
 	pid := int(payload.PID)
 	enrichment := tcpLifecycleEnrichment{}
 	trackedProcess := false
 
 	if enricher.processes != nil {
-		snapshot, found, tracked := enricher.processes.Lookup(
-			payload.PID,
-			payload.UID,
-			payload.Comm,
-			payload.KernelTimestampNS,
-		)
+		snapshot, found, tracked := enricher.processes.Lookup(payload.PID, payload.UID, payload.Comm, payload.KernelTimestampNS)
 		trackedProcess = tracked
 		if found {
 			enrichment.ProcessPath = snapshot.Executable
 			enrichment.User = snapshot.User
 			enrichment.GID = uint32Pointer(snapshot.GID)
-			applyProcessContext(
-				&enrichment,
-				snapshot.Context,
-				uint64PointerIfNonZero(snapshot.CgroupID),
-			)
+			applyProcessContext(&enrichment, snapshot.Context, uint64PointerIfNonZero(snapshot.CgroupID))
 			if enricher.includeExtendedFields {
 				enrichment.ProcessArgs = snapshot.Arguments
 			}
@@ -176,7 +149,6 @@ func (enricher *tcpLifecycleEnricher) lookup(
 		if enrichment.ProcessPath == "" {
 			enrichment.ProcessPath = enricher.lookups.processPath(pid)
 		}
-
 		if enricher.lookups.context != nil {
 			context := enricher.lookups.context(pid)
 			if enrichment.GID == nil {
@@ -189,180 +161,107 @@ func (enricher *tcpLifecycleEnricher) lookup(
 	if enrichment.User == "" {
 		enrichment.User = enricher.lookups.username(payload.UID)
 	}
+	if enricher.lookups.dns != nil {
+		enrichment.DNS = enricher.lookups.dns(payload)
+	}
 
 	if !enricher.includeExtendedFields {
 		return enrichment
 	}
-
 	if enrichment.ProcessArgs == "" && !trackedProcess {
 		enrichment.ProcessArgs = enricher.lookups.processArgs(pid)
 	}
-	enrichment.ASN = enricher.lookups.asn(payload)
-
+	if enricher.lookups.asn != nil {
+		enrichment.ASN = enricher.lookups.asn(payload)
+	}
 	return enrichment
 }
 
-func applyProcessContext(
-	enrichment *tcpLifecycleEnrichment,
-	context processContextSnapshot,
-	cgroupID *uint64,
-) {
+func applyProcessContext(enrichment *tcpLifecycleEnrichment, context processContextSnapshot, cgroupID *uint64) {
 	if context.StartTimeTicks != 0 {
 		enrichment.ProcessStartTimeTicks = uint64Pointer(context.StartTimeTicks)
 	}
-
 	if context.ParentPID != 0 {
-		parent := &tcpLifecycleProcessParentPayload{
-			PID: context.ParentPID,
-		}
+		parent := &tcpLifecycleProcessParentPayload{PID: context.ParentPID}
 		if context.ParentStartTimeTicks != 0 {
 			parent.StartTimeTicks = uint64Pointer(context.ParentStartTimeTicks)
 		}
 		enrichment.Parent = parent
 	}
-
 	if cgroupID != nil || context.CgroupPath != "" {
-		enrichment.Cgroup = &tcpLifecycleCgroupPayload{
-			ID:   cloneUint64Pointer(cgroupID),
-			Path: context.CgroupPath,
-		}
+		enrichment.Cgroup = &tcpLifecycleCgroupPayload{ID: cloneUint64Pointer(cgroupID), Path: context.CgroupPath}
 	}
-
 	if !processNamespacesEmpty(context.Namespaces) {
 		enrichment.Namespaces = &tcpLifecycleNamespacesPayload{
 			Cgroup: context.Namespaces.Cgroup,
-			IPC:    context.Namespaces.IPC,
-			Mount:  context.Namespaces.Mount,
-			Net:    context.Namespaces.Net,
-			PID:    context.Namespaces.PID,
-			User:   context.Namespaces.User,
-			UTS:    context.Namespaces.UTS,
+			IPC: context.Namespaces.IPC,
+			Mount: context.Namespaces.Mount,
+			Net: context.Namespaces.Net,
+			PID: context.Namespaces.PID,
+			User: context.Namespaces.User,
+			UTS: context.Namespaces.UTS,
 		}
 	}
-
 	if context.Container != nil {
-		enrichment.Container = &tcpLifecycleContainerPayload{
-			Runtime: context.Container.Runtime,
-			ID:      context.Container.ID,
-		}
+		enrichment.Container = &tcpLifecycleContainerPayload{Runtime: context.Container.Runtime, ID: context.Container.ID}
 	}
 }
 
 func uint64PointerIfNonZero(value uint64) *uint64 {
-	if value == 0 {
-		return nil
-	}
+	if value == 0 { return nil }
 	return uint64Pointer(value)
 }
 
 func lookupTCPLifecycleUsername(uid uint32) string {
 	uidText := strconv.FormatUint(uint64(uid), 10)
-
-	if userInfo, err := user.LookupId(uidText); err == nil {
-		return userInfo.Username
-	}
-
+	if userInfo, err := user.LookupId(uidText); err == nil { return userInfo.Username }
 	return uidText
 }
 
-func lookupTCPLifecycleASN(
-	payload tcpLifecycleEventPayload,
-) *tcpLifecycleASNPayload {
-	if payload.Remote.IP == nil {
-		return nil
-	}
-
+func lookupTCPLifecycleASN(payload tcpLifecycleEventPayload) *tcpLifecycleASNPayload {
+	if payload.Remote.IP == nil { return nil }
 	if payload.Remote.IP.To4() != nil {
 		info := as.GetASInfoIPv4(payload.Remote.IP)
-		if info.AsNumber == 0 {
-			return nil
-		}
-
-		return &tcpLifecycleASNPayload{
-			Number: info.AsNumber,
-			Name:   info.Name,
-		}
+		if info.AsNumber == 0 { return nil }
+		return &tcpLifecycleASNPayload{Number: info.AsNumber, Name: info.Name}
 	}
-
 	if payload.Remote.IP.To16() != nil {
 		info := as.GetASInfoIPv6(payload.Remote.IP)
-		if info.AsNumber == 0 {
-			return nil
-		}
-
-		return &tcpLifecycleASNPayload{
-			Number: info.AsNumber,
-			Name:   info.Name,
-		}
+		if info.AsNumber == 0 { return nil }
+		return &tcpLifecycleASNPayload{Number: info.AsNumber, Name: info.Name}
 	}
-
 	return nil
 }
 
-func cloneTCPLifecycleParent(
-	value *tcpLifecycleProcessParentPayload,
-) *tcpLifecycleProcessParentPayload {
-	if value == nil {
-		return nil
-	}
-
-	return &tcpLifecycleProcessParentPayload{
-		PID:            value.PID,
-		StartTimeTicks: cloneUint64Pointer(value.StartTimeTicks),
-	}
+func lookupTCPLifecycleDNS(payload tcpLifecycleEventPayload) *dnsCorrelationPayload {
+	return lookupDNSCorrelation(payload.Remote.IP, payload.PID, payload.ObservedAt)
 }
 
-func cloneTCPLifecycleCgroup(
-	value *tcpLifecycleCgroupPayload,
-) *tcpLifecycleCgroupPayload {
-	if value == nil {
-		return nil
-	}
-
-	return &tcpLifecycleCgroupPayload{
-		ID:   cloneUint64Pointer(value.ID),
-		Path: value.Path,
-	}
+func cloneTCPLifecycleParent(value *tcpLifecycleProcessParentPayload) *tcpLifecycleProcessParentPayload {
+	if value == nil { return nil }
+	return &tcpLifecycleProcessParentPayload{PID: value.PID, StartTimeTicks: cloneUint64Pointer(value.StartTimeTicks)}
 }
-
-func cloneTCPLifecycleNamespaces(
-	value *tcpLifecycleNamespacesPayload,
-) *tcpLifecycleNamespacesPayload {
-	if value == nil {
-		return nil
-	}
-
+func cloneTCPLifecycleCgroup(value *tcpLifecycleCgroupPayload) *tcpLifecycleCgroupPayload {
+	if value == nil { return nil }
+	return &tcpLifecycleCgroupPayload{ID: cloneUint64Pointer(value.ID), Path: value.Path}
+}
+func cloneTCPLifecycleNamespaces(value *tcpLifecycleNamespacesPayload) *tcpLifecycleNamespacesPayload {
+	if value == nil { return nil }
 	copied := *value
 	return &copied
 }
-
-func cloneTCPLifecycleContainer(
-	value *tcpLifecycleContainerPayload,
-) *tcpLifecycleContainerPayload {
-	if value == nil {
-		return nil
-	}
-
+func cloneTCPLifecycleContainer(value *tcpLifecycleContainerPayload) *tcpLifecycleContainerPayload {
+	if value == nil { return nil }
 	copied := *value
 	return &copied
 }
-
-func cloneTCPLifecycleASN(
-	value *tcpLifecycleASNPayload,
-) *tcpLifecycleASNPayload {
-	if value == nil {
-		return nil
-	}
-
+func cloneTCPLifecycleASN(value *tcpLifecycleASNPayload) *tcpLifecycleASNPayload {
+	if value == nil { return nil }
 	copied := *value
 	return &copied
 }
-
 func cloneUint32Pointer(value *uint32) *uint32 {
-	if value == nil {
-		return nil
-	}
-
+	if value == nil { return nil }
 	copied := *value
 	return &copied
 }
